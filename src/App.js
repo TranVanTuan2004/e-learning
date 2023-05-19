37fd114689fa8e9b0e08ad6a99a236a229137909
Tuấn Trần <tuantran280504@gmail.com>
2023-05-19 15:45:34 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { authenRoutes } from "./routes/authenRoutes";
+import { clientRoutes } from "./routes/clientRoutes";
+import HomePage from "./pages/HomePage/HomePage";
+import { adminRoutes } from "./routes/adminRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-primary text-second">
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        {clientRoutes.map(({url, component}) => {
+          return <Route key={url} path={url} element={component}/>
+        })}
+        {authenRoutes.map(({url, component}) => {
+          return <Route key={url} path={url} element={component}/>
+        })}
+        {adminRoutes.map(({url, component}) => {
+          return <Route key={url} path={url} element={component}/>
+        })}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
